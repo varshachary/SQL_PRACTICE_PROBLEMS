ab7d8626a431281f7fd34caa6d0b740c90bcf2ac
Varsha Chary <34280450+varshachary@users.noreply.github.com>
2021-05-18 11:27:19 +0530

    Update email_duplicate.sql

--- email_duplicate.sql
+++ email_duplicate.sql
@@ -1,5 +1,6 @@
 /*
 Write a SQL query to find all duplicate emails in a table named Person.
+Source:Leetcode
 */
 # Write your MySQL query statement below
 select Email from Person group by Email having count(Email)>1
